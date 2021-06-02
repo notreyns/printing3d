5ae67f796519be4f99aac280e47bbd14e41dfcd3
Reina <reina.khasanova@iaau.edu.kg>
2021-06-03 01:31:18 +0600

    initial commit

---
+++ src/main/java/com/example/printing3d/Printing3dApplication.java
@@ -0,0 +1,13 @@
+package com.example.printing3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Printing3dApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Printing3dApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/printing3d/Printing3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.printing3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Printing3dApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
