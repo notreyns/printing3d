6caf7a620c9e4a4a89466e21cc803ddc25fff74a
Reina <reina.khasanova@iaau.edu.kg>
2021-06-03 17:53:42 +0600

    deleting post implemented

--- src/main/java/com/example/printing3d/controllers/MainController.java
+++ src/main/java/com/example/printing3d/controllers/MainController.java
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
 
 @Controller
 @RequestMapping("/")
